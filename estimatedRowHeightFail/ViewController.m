804a8ae0fbc32cd0eb60ac934a4db08de222c023
Oisin Prendiville <oisin@supertop.co>
2015-07-16 21:10:16 +0200

    Updates for iOS 9 and UITableViewAutomaticDimension

--- estimatedRowHeightFail/ViewController.m
+++ estimatedRowHeightFail/ViewController.m
@@ -22,44 +22,34 @@
     [self.tableView scrollToRowAtIndexPath:lastRow
                           atScrollPosition:UITableViewScrollPositionBottom
                                   animated:YES];
+}
+
+- (IBAction)jumpToTop:(id)sender {
+    NSIndexPath *firstRow = [NSIndexPath indexPathForRow:0 inSection:0];
     
-    // FIRST BUG
-    // DESCRIPTION: scrollToRowAtIndexPath:atScrollPosition:animated: is broken when using estimatedRowHeight.
+    [self.tableView scrollToRowAtIndexPath:firstRow
+                          atScrollPosition:UITableViewScrollPositionTop
+                                  animated:NO];
     
-    // Tap "Go To Bottom"
-    // if the total for all estimated heights is > actual heights, we zoom past the end and get a blank screen.
-    // if the total for all estimated heights is < actual heights, we don't reach the end — the scroller is not 100% of the way.
-    
-    // The results differ between `animated` being YES or NO, but neither is correct.
-    
-    // SECOND BUG:
-    // DESCRIPTION: After scrolling past a cell that never gets its heightForRowAtIndexPath called, cells drawn when scrolling back up flicker wildly.
-    
-    // 1. Tap "Go To Bottom" once — it'll end up halfway down the table thanks to the first bug, but that's fine for now.
-    // 2. Using your finger, start slowly scrolling back up towards the top of the table.
-    // 3. After scrolling 10-15 cells the cells all suddenly scroll much faster than they should be.
-    // 4. Things are normal again until you scroll up another 10 or so cells...
-    
-    // NOTE: Manually scrolling fast enough to skip a cell causes this too, but the scrollToRowAtIndexPath method here illustrates it easiest.
-    // The wider the difference between estRowHeight and actualHeight, the worse this gets — it is noticable even with 5pt differences though.
 }
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.numberOfRows = 1000;
     
-    self.tableView.estimatedRowHeight = 20;
+    self.tableView.estimatedRowHeight = 200;
+    self.tableView.rowHeight = UITableViewAutomaticDimension;
+}
+
+- (BOOL)prefersStatusBarHidden
+{
+    return NO;
 }
 
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
     return self.numberOfRows;
-}
-
-- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    return 100.0f;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
